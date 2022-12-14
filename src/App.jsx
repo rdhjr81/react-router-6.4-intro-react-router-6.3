a0278343418084494b023e26ec220bd60829a585
Rob Horner <rdhjr81@gmail.com>
2022-12-14 12:20:16 -0500

    initial

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes, createRoutesFromElements } from 'react-router-dom';
+
+import BlogLayout from './pages/BlogLayout';
+import BlogPostsPage, {loader as blogPostsLoader} from './pages/BlogPosts';
+import NewPostPage from './pages/NewPost';
+import PostDetailPage from './pages/PostDetail';
+import RootLayout from './pages/RootLayout';
+import WelcomePage from './pages/Welcome';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path ="/" element={<RootLayout />}>
+       <Route index element={<WelcomePage />} />
+      <Route path="/blog" element={<BlogLayout />}>
+        <Route index element={<BlogPostsPage />} loader={blogPostsLoader}/>
+        <Route path=":id" element={<PostDetailPage />} />
+      </Route>
+      <Route path="/blog/new" element={<NewPostPage />} /> 
+    </Route>
+    )
+  );
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
